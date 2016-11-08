ec53f9c9dc53bd85999a4ba5fc3a0621bdaf764e
sowmith1 <sowmith001@gmail.com>
2016-11-08 14:41:42 -0500

    this is my first commit

---
+++ chaseBanking/src/main/java/com/chase/bank/App.java
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+package com.chase.bank;
+
+/**
+ * Hello worlddddddddddd!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+=======
+package com.chase.bank;
+
+/**
+ * Hello worldddddd!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+>>>>>>> 063677a065f08f0c106fb9b212af138a80a7d671
---
+++ chaseBanking/src/test/java/com/chase/bank/AppTest.java
@@ -0,0 +1,79 @@
+<<<<<<< HEAD
+package com.chase.bank;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+=======
+package com.chase.bank;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+>>>>>>> 063677a065f08f0c106fb9b212af138a80a7d671
